b519439619a1636ae72cbbb8c47bd550a5b53da4
Thomas Preymesser <thopre@gmail.com>
2017-03-19 15:51:11 +0100

    Update F1.rs

--- F1.rs
+++ F1.rs
@@ -1,2 +1,73 @@
-main {    println!("this is formel 1")
+main {    println!("this is formel 1");
+  
+  Ich stimme zu
+Formel1.de verwendet Cookies, um Dir den bestmöglichen Service anbieten zu können. Wenn Du auf der Seite weitersurfst, stimmst Du der Cookie-Nutzung zu. Weitere Infos
+
+ 
+Startseite Menü
+
+Suchen
+Apps Newsletter RSS Forum
+News Fotos & Videos Ergebnisse Kalender Teams & Fahrer Fanshop & Tickets Mehr... präsentiert von AutoBild.de
+
+Home  >  Formel-1-Liveticker
+Australien, Melbourne 1. Freies Training Live-WM-StandStrecken-Porträt
+Formel-1-Liveticker
+Großer Preis von Australien · 1. Freies Training · Verbleibende Minuten: 90
+
+Wetter: Keine Informationen · Strecke: Keine Informationen · Flaggensignal: aktualisieren
+F. Alonso
+F. Alonso
+McLaren
+-:--:---	S. Perez
+S. Perez
+Force India
+-:--:---	M. Ericsson
+M. Ericsson
+Sauber
+-:--:---	R. Grosjean
+R. Grosjean
+Haas
+-:--:---	K. Räikkönen
+K. Räikkönen
+Ferrari
+-:--:---	S. Vettel
+S. Vettel
+Ferrari
+-:--:---	D. Ricciardo
+D. Ricciardo
+Red Bull
+-:--:---	S. Vandoorne
+S. Vandoorne
+McLaren
+Pos.	Fahrer	Team	Zeit	Abstand	 
+1	S. Vandoorne	McLaren	-:--.---		0
+2	D. Ricciardo	Red Bull	-:--.---	-:--.---	0
+3	S. Vettel	Ferrari	-:--.---	-:--.---	0
+4	K. Räikkönen	Ferrari	-:--.---	-:--.---	0
+5	R. Grosjean	Haas	-:--.---	-:--.---	0
+6	M. Ericsson	Sauber	-:--.---	-:--.---	0
+7	S. Perez	Force India	-:--.---	-:--.---	0
+8	F. Alonso	McLaren	-:--.---	-:--.---	0
+9	L. Stroll	Williams	-:--.---	-:--.---	0
+10	F. Massa	Williams	-:--.---	-:--.---	0
+11	K. Magnussen	Haas	-:--.---	-:--.---	0
+12	D. Kwjat	Toro Rosso	-:--.---	-:--.---	0
+13	N. Hülkenberg	Renault	-:--.---	-:--.---	0
+14	J. Palmer	Renault	-:--.---	-:--.---	0
+15	E. Ocon	Force India	-:--.---	-:--.---	0
+16	M. Verstappen	Red Bull	-:--.---	-:--.---	0
+17	L. Hamilton	Mercedes	-:--.---	-:--.---	0
+18	C. Sainz	Toro Rosso	-:--.---	-:--.---	0
+19	V. Bottas	Mercedes	-:--.---	-:--.---	0
+20	P. Wehrlein	Sauber	-:--.---	-:--.---	0Fahrer ausgeschiedenLive-Zeiten von F1.com anzeigen
+11:51
+Wir berichten live vom 1. Freien Training am 24. März ab 1:55 Uhr.
+
+Alles anzeigen
+teilentwitternteilenWhatsAppMailForum
+Home Impressum Kontakt Nutzungsbedingungen Datenschutz & Cookies Mediadaten
+© Copyright 1‌996 - ‌2017 sport media group GmbH, München
+This website is unofficial and is not associated in any way with the Formula One group of companies. F1, FORMULA ONE, FORMULA 1, FIA FORMULA ONE WORLD CHAMPIONSHIP, FORMEL 1, GRAND PRIX and related marks are trade marks of Formula One Licensing B.V. The trade mark FORMEL 1 is used under license. All rights reserved.  
+  
 }
